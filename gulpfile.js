253cfa76daa3077a869113e8d4f0ad81c55ccf09
Nigel Marshal <nigel@Alissa.local>
2018-05-04 19:23:54 +0400

    Finished header js functionality. Had to re add files to fix npm errors.

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,3 @@ require('./gulp/tasks/scripts');
 
 
 
-
-
-
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-var path = require('path');
-
 module.exports = {
-	entry: "./app/assets/scripts/app.js",
-	output: {
-		path: path.resolve(__dirname, "./app/temp/scripts"),
-		filename: "compiled-app.js"
-	}
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: "./app/temp/scripts",
+    filename: "App.js"
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
